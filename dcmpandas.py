033597a3aad652957c0a78f06526e5aa521130d8
Dov Grobgeld <dov.grobgeld@gmail.com>
2017-05-07 21:28:20 +0300

    Added recursive traversal (default) and module documentation.

--- dcmpandas.py
+++ dcmpandas.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python
+'''dcmpandas is a scraper and query system for interacting with a 
+directory of dicom images. It builds a database of the tags
+of all images in the database, and then allows querying 
+these tags through the flexible pandas syntax.
+
+The following example imports dcmpandas and builds a database of all
+dicom images in the current directory. (Obviously the building of the
+database only needs to be performed once.) It then loads the database
+into memory, and performs a few queries and views the images of one
+series.
+
+    In [1]: import dcmpandas as dp
+    
+    In [2]: dp.scrape('.')
+    Processing 48862992
+    Processing 48857684
+    Processing 48858202
+    Processing 48862824
+    :
+    In [3]: tags,df = dp.load()
+    In [4]: df.SeriesDescription.drop_duplicates()
+    Out[4]: 
+    0                   Ax t2_tse_spair
+    1                         Ax t2_tse
+    2                      Ax 3D GRE T1
+    180            Ax Diff ep2d_mddw_30
+    181    Ax Diff ep2d_mddw_30_a/p rpt
+    Name: SeriesDescription, dtype: object
+'''
+
+
 #
 # a utility library for reading dicom files and place their
 # information in a pandas database.
@@ -26,9 +57,10 @@
 import os
 import sys
 import glob
-import dicom
+import pydicom as dicom
 import pandas
 import pickle
+import fnmatch
 
 # Default viewer is giv. See github.com/dov/giv .
 viewer = 'giv'
@@ -52,7 +84,8 @@ def scrape(directory = '.',
            glob_pattern = '*',
            verbose=1,
            guess_convert = True,
-           sort_slice_location = True):
+           sort_slice_location = True,
+           recursive = True):
     """Scrape a directory of data
 
     Flags:
@@ -63,53 +96,59 @@ def scrape(directory = '.',
     db = []
     s = []
     tags = {}
-    for f in glob.glob(glob_pattern):
-        if not is_dicom(f):
-            continue
-        
+    for root, dirnames, filenames in os.walk(directory):
         if verbose:
-            print 'Processing',f
-        ds = dicom.read_file(f,defer_size='10KB')
-        h = {}
-        h['Filename'] = f
-        for k in ds.keys():
-            # Skip images
-            if (k.group,k.elem) == (0x7fe0,0x0010):
+            print 'Visiting ' + root
+        if root != directory and not recursive:
+            continue
+        for fn in fnmatch.filter(filenames, glob_pattern):
+            f = os.path.join(root, fn)
+            if not is_dicom(f):
                 continue
-            v = ds[k]
-            key = (v.name
-                   .replace(' ','')
-                   .replace('\'','')
-                   .replace('/','')
-                   .replace('[','')
-                   .replace(']','')
-                   .replace('(','')
-                   .replace(')','')
-                   )
-            # TBD - interpret VR and act accordingly
-            value = str(v.value)
-
-            # Carry out some common convertion.
-            if guess_convert:
-                if (k.group,k.elem) in [(0x20,0x1041), # Slice location
-                                        (0x18,0x50)    # Slice thickness
-                                        ]:
-                    value = float(value)
-                elif (k.group,k.elem) in [(0x28,0x30), # Pixel spacing
-                                        ]:
-                    value = [float(f) for f in v.value]
-
-                elif not ('\\' in value or '[' in value):
-                    if v.VR in ['IS','SL','US']:
-                        value = int(value)
-
-            h[key] = value
-            h['X%04x_%04x'%(k.group,k.elem)]=h[key] # Use both name and group,element syntax
-            tags[key] = (k.group,
-                         k.element,
-                         '(%04x_%04x)'%(k.group,k.elem),
-                         v.VR)
-        db += [h]
+            
+            if verbose:
+                print 'Processing',f
+            ds = dicom.read_file(f,defer_size='10KB')
+            h = {}
+            h['Filename'] = f
+            for k in ds.keys():
+                # Skip images
+                if (k.group,k.elem) == (0x7fe0,0x0010):
+                    continue
+                v = ds[k]
+                key = (v.name
+                       .replace(' ','')
+                       .replace('\'','')
+                       .replace('/','')
+                       .replace('[','')
+                       .replace(']','')
+                       .replace('(','')
+                       .replace(')','')
+                       )
+                # TBD - interpret VR and act accordingly
+                value = str(v.value)
+    
+                # Carry out some common convertion.
+                if guess_convert:
+                    if (k.group,k.elem) in [(0x20,0x1041), # Slice location
+                                            (0x18,0x50)    # Slice thickness
+                                            ]:
+                        value = float(value)
+                    elif (k.group,k.elem) in [(0x28,0x30), # Pixel spacing
+                                            ]:
+                        value = [float(f) for f in v.value]
+    
+                    elif not ('\\' in value or '[' in value):
+                        if v.VR in ['IS','SL','US']:
+                            value = int(value)
+    
+                h[key] = value
+                h['X%04x_%04x'%(k.group,k.elem)]=h[key] # Use both name and group,element syntax
+                tags[key] = (k.group,
+                             k.element,
+                             '(%04x_%04x)'%(k.group,k.elem),
+                             v.VR)
+            db += [h]
         
     # Create a dataset of everything
     if sort_slice_location:
